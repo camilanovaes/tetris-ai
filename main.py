963996903a83a40e3436cb1de02a0aa6d8051a71
Camila Novaes <novaes.csw@gmail.com>
2019-12-07 16:29:07 -0300

    Fix bugs

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import tetris_ai.tetris_ai as t
 import tetris_ai.ga as ga
-import tetris_ai.tetris_base as tetris
+import tetris_ai.tetris_base as game
+import tetris_ai.tetris_ai as ai
 import matplotlib.pyplot as plt
 import argparse
 
-def main():
+def main(no_show):
     # GENERAL CONFIG
-    NUM_GEN        = 5
-    NUM_POP        = 15
+    NUM_GEN        = 3
+    NUM_POP        = 3
     MUTATION_RATE  = 0.3
-    CROSSOVER_RATE = 0.5
-    N_SELECTION    = 5
+    CROSSOVER_RATE = 0.75
 
     genetic_alg    = ga.GA(NUM_POP)
 
@@ -25,14 +24,6 @@ def main():
         print (f' - - - - Geração atual: {j+1} - - - - ')
         print (' \n')
 
-        for i in range(NUM_POP):
-            # Run the game for each chromosome
-            game_state = t.run_game(genetic_alg.chromosomes[i], game_speed, \
-                                    max_score = 200000, show_game = False)
-            # Calculate the fitness
-            genetic_alg.chromosomes[i].calc_fitness(game_state)
-            print(f"Individuo: {(i + 1)}, Score: {genetic_alg.chromosomes[i].score}")
-
         # Select chromosomes using roulette method
         selected_pop = genetic_alg.selection(genetic_alg.chromosomes, NUM_POP, \
                                              type="roulette")
@@ -42,6 +33,19 @@ def main():
                                             mutation="random", \
                                             crossover_rate=CROSSOVER_RATE, \
                                             mutation_rate=MUTATION_RATE)
+
+        for i in range(NUM_POP):
+            # Run the game for each chromosome
+            game_state = ai.run_game(genetic_alg.chromosomes[i], game_speed, \
+                                    max_score = 200000, no_show = no_show)
+            # Calculate the fitness
+            genetic_alg.chromosomes[i].calc_fitness(game_state)
+
+            # Print information
+            print(f"Individuo: {(i + 1)}:")
+            print(f"   Score: {genetic_alg.chromosomes[i].score}")
+            print(f"   Weights: {genetic_alg.chromosomes[i].weights}")
+            print('--')
 
         # Insert new children in pop
         genetic_alg.chromosomes[-(len(new_chromo)):] = new_chromo
@@ -57,22 +61,25 @@ if __name__ == "__main__":
     parser.add_argument('--game',
                         action='store_true',
                         help='Run the base game without AI')
+    parser.add_argument('--no-show',
+                        action='store_true',
+                        help='Whether to show the game')
 
     args = parser.parse_args()
 
     if (args.train):
         # Train the AI and after play the game with the get chromosome
-        best_chromos = main()
+        best_chromos = main(args.no_show)
 
     elif (args.game):
         # Just run the base game
-        tetris.MANUAL_GAME = True
-        tetris.main()
+        game.MANUAL_GAME = True
+        game.main()
 
     else:
         # Run tetris AI with optimal weights
         # FIXME: Define the optimal weights
         optimal_weights = [-0.97, 5.47, -13.74, -0.73,  7.99, -0.86, -0.72]
         chromo = ga.Chromosome(optimal_weights)
-        t.run_game(chromo, speed=600, max_score=200000, show_game=True)
+        ai.run_game(chromo, speed=600, max_score=200000, no_show=False)
 
